6808b47ae32290ed603694261706b1156adafc9f
Andrew Waterman <waterman@cs.berkeley.edu>
2015-09-11 15:49:04 -0700

    Chisel3 compatibility fixes

--- src/main/scala/sram.scala
+++ src/main/scala/sram.scala
@@ -7,23 +7,22 @@ class HASTISRAM(depth: Int) extends Module with HASTIConstants
 {
   val io = new HASTISlaveIO
 
-  val ram = SeqMem(Bits(width = SZ_HDATA), depth)
+  val wdata = Reg(Vec(Bits(width = 8), SZ_HDATA/8))
   val waddr = Reg(UInt(width = SZ_HADDR))
-  val wdata = Reg(Bits(width = SZ_HDATA))
   val wvalid = Reg(init = Bool(false))
   val wsize = Reg(UInt(width = SZ_HSIZE))
+  val ram = SeqMem(wdata, depth)
 
   val wmask_lut = MuxLookup(wsize, Bits(0xf), Seq(
         UInt(0) -> Bits(0x1),
         UInt(1) -> Bits(0x3)))
-  val wmask_shift = wmask_lut << waddr(1,0)
-  val wmask = FillInterleaved(8, wmask_shift)
+  val wmask = wmask_lut << waddr(1,0)
 
   val s_w1 :: s_w2 :: Nil = Enum(UInt(), 2)
   val state = Reg(init = s_w1)
 
   when (state === s_w2) {
-    wdata := io.hwdata
+    wdata := Vec.tabulate(SZ_HDATA/8)(i => io.hwdata(8*(i+1)-1,8*i))
     state := s_w1
   }
 
@@ -39,7 +38,7 @@ class HASTISRAM(depth: Int) extends Module with HASTIConstants
       wsize := io.hsize
       wvalid := Bool(true)
       when (wvalid) {
-        ram.write(waddr >> UInt(2), wdata, wmask)
+        ram.write(waddr >> UInt(2), wdata, wmask.toBools)
       }
       state := s_w2
     } .otherwise {
@@ -48,10 +47,9 @@ class HASTISRAM(depth: Int) extends Module with HASTIConstants
     }
   }
 
-  val rdata = ram.read(raddr, ren)
-  val rmask = FillInterleaved(8,
-    wmask_shift & Fill(SZ_HDATA / 8, bypass))
-  io.hrdata := (wdata & rmask) | (rdata & ~rmask)
+  val rdata = ram.read(raddr, ren).toBits
+  val rmask = FillInterleaved(8, wmask & Fill(SZ_HDATA / 8, bypass))
+  io.hrdata := (wdata.toBits & rmask) | (rdata & ~rmask)
 
   io.hreadyout := Bool(true)
   io.hresp := HRESP_OKAY
