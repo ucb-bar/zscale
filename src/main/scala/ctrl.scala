b680f11c257dcec00a22cc6b15eb736c7059a6ed
Andrew Waterman <waterman@cs.berkeley.edu>
2015-09-28 11:54:26 -0700

    Improve decode table
    - Get rid of shift amount hack (requires updating rocket repo)
    - Add x-register read-enable signals (to be used later)

--- src/main/scala/ctrl.scala
+++ src/main/scala/ctrl.scala
@@ -88,82 +88,82 @@ class Control extends Module with ZscaleParameters
     id_valid := !io.dpath.killf
   }
 
-                //  val j br si f.i csr    s_alu1   s_alu2   imm     fn       wen sb mem rw mtype  mul
-                //   |  |  |  |  |  |      |        |        |       |          |  |  |  |  |      |
+                  //  val j br f.i csr    s_alu1   s_alu2   imm     fn       wen r1 r2 sb mem rw mtype  mul
+                  //   |  |  |  |  |      |        |        |       |          |  |  |  |  |  |  |      |
   val decode_default: List[BitPat] =
-                List(N, X, X, X, X, CSR.X, A1_X,    A2_X,    IMM_X,  FN_X,      X, X, X, X, MT_X,  X)
+                  List(N, X, X, X, CSR.X, A1_X,    A2_X,    IMM_X,  FN_X,      X, X, X, X, X, X, MT_X,  X)
   val decode_table: Array[(BitPat, List[BitPat])] = Array(
-      LUI->     List(Y, N, N, N, N, CSR.N, A1_ZERO, A2_IMM,  IMM_U,  FN_ADD,    Y, N, N, X, MT_X,  N),
-      AUIPC->   List(Y, N, N, N, N, CSR.N, A1_PC,   A2_IMM,  IMM_U,  FN_ADD,    Y, N, N, X, MT_X,  N),
+      LUI->       List(Y, N, N, N, CSR.N, A1_ZERO, A2_IMM,  IMM_U,  FN_ADD,    Y, N, N, N, N, X, MT_X,  N),
+      AUIPC->     List(Y, N, N, N, CSR.N, A1_PC,   A2_IMM,  IMM_U,  FN_ADD,    Y, N, N, N, N, X, MT_X,  N),
 
-      JAL->     List(Y, Y, N, N, N, CSR.N, A1_PC,   A2_FOUR, IMM_UJ, FN_ADD,    Y, N, N, X, MT_X,  N),
-      JALR->    List(Y, Y, N, N, N, CSR.N, A1_PC,   A2_FOUR, IMM_I,  FN_ADD,    Y, N, N, X, MT_X,  N),
+      JAL->       List(Y, Y, N, N, CSR.N, A1_PC,   A2_FOUR, IMM_UJ, FN_ADD,    Y, N, N, N, N, X, MT_X,  N),
+      JALR->      List(Y, Y, N, N, CSR.N, A1_PC,   A2_FOUR, IMM_I,  FN_ADD,    Y, Y, N, N, N, X, MT_X,  N),
 
-      BEQ->     List(Y, N, Y, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_SB, FN_SEQ,    N, N, N, X, MT_X,  N),
-      BNE->     List(Y, N, Y, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_SB, FN_SNE,    N, N, N, X, MT_X,  N),
-      BLT->     List(Y, N, Y, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_SB, FN_SLT,    N, N, N, X, MT_X,  N),
-      BLTU->    List(Y, N, Y, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_SB, FN_SLTU,   N, N, N, X, MT_X,  N),
-      BGE->     List(Y, N, Y, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_SB, FN_SGE,    N, N, N, X, MT_X,  N),
-      BGEU->    List(Y, N, Y, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_SB, FN_SGEU,   N, N, N, X, MT_X,  N),
+      BEQ->       List(Y, N, Y, N, CSR.N, A1_RS1,  A2_RS2,  IMM_SB, FN_SEQ,    N, Y, Y, N, N, X, MT_X,  N),
+      BNE->       List(Y, N, Y, N, CSR.N, A1_RS1,  A2_RS2,  IMM_SB, FN_SNE,    N, Y, Y, N, N, X, MT_X,  N),
+      BLT->       List(Y, N, Y, N, CSR.N, A1_RS1,  A2_RS2,  IMM_SB, FN_SLT,    N, Y, Y, N, N, X, MT_X,  N),
+      BLTU->      List(Y, N, Y, N, CSR.N, A1_RS1,  A2_RS2,  IMM_SB, FN_SLTU,   N, Y, Y, N, N, X, MT_X,  N),
+      BGE->       List(Y, N, Y, N, CSR.N, A1_RS1,  A2_RS2,  IMM_SB, FN_SGE,    N, Y, Y, N, N, X, MT_X,  N),
+      BGEU->      List(Y, N, Y, N, CSR.N, A1_RS1,  A2_RS2,  IMM_SB, FN_SGEU,   N, Y, Y, N, N, X, MT_X,  N),
 
-      LB->      List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_ADD,    N, Y, Y, N, MT_B,  N),
-      LBU->     List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_ADD,    N, Y, Y, N, MT_BU, N),
-      LH->      List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_ADD,    N, Y, Y, N, MT_H,  N),
-      LHU->     List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_ADD,    N, Y, Y, N, MT_HU, N),
-      LW->      List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_ADD,    N, Y, Y, N, MT_W,  N),
-      SB->      List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_S,  FN_ADD,    N, Y, Y, Y, MT_B,  N),
-      SH->      List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_S,  FN_ADD,    N, Y, Y, Y, MT_H,  N),
-      SW->      List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_S,  FN_ADD,    N, Y, Y, Y, MT_W,  N),
+      LB->        List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_ADD,    N, Y, N, Y, Y, N, MT_B,  N),
+      LBU->       List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_ADD,    N, Y, N, Y, Y, N, MT_BU, N),
+      LH->        List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_ADD,    N, Y, N, Y, Y, N, MT_H,  N),
+      LHU->       List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_ADD,    N, Y, N, Y, Y, N, MT_HU, N),
+      LW->        List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_ADD,    N, Y, N, Y, Y, N, MT_W,  N),
+      SB->        List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_S,  FN_ADD,    N, Y, Y, Y, Y, Y, MT_B,  N),
+      SH->        List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_S,  FN_ADD,    N, Y, Y, Y, Y, Y, MT_H,  N),
+      SW->        List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_S,  FN_ADD,    N, Y, Y, Y, Y, Y, MT_W,  N),
 
-      ADDI->    List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_ADD,    Y, N, N, X, MT_X,  N),
-      SLTI->    List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_SLT,    Y, N, N, X, MT_X,  N),
-      SLTIU->   List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_SLTU,   Y, N, N, X, MT_X,  N),
-      XORI->    List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_XOR,    Y, N, N, X, MT_X,  N),
-      ORI->     List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_OR,     Y, N, N, X, MT_X,  N),
-      ANDI->    List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_AND,    Y, N, N, X, MT_X,  N),
-      SLLI->    List(Y, N, N, Y, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_SL,     Y, N, N, X, MT_X,  N),
-      SRLI->    List(Y, N, N, Y, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_SR,     Y, N, N, X, MT_X,  N),
-      SRAI->    List(Y, N, N, Y, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_SRA,    Y, N, N, X, MT_X,  N),
+      ADDI->      List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_ADD,    Y, Y, N, N, N, X, MT_X,  N),
+      SLTI->      List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_SLT,    Y, Y, N, N, N, X, MT_X,  N),
+      SLTIU->     List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_SLTU,   Y, Y, N, N, N, X, MT_X,  N),
+      XORI->      List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_XOR,    Y, Y, N, N, N, X, MT_X,  N),
+      ORI->       List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_OR,     Y, Y, N, N, N, X, MT_X,  N),
+      ANDI->      List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_AND,    Y, Y, N, N, N, X, MT_X,  N),
+      SLLI_RV32-> List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_SL,     Y, Y, N, N, N, X, MT_X,  N),
+      SRLI_RV32-> List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_SR,     Y, Y, N, N, N, X, MT_X,  N),
+      SRAI_RV32-> List(Y, N, N, N, CSR.N, A1_RS1,  A2_IMM,  IMM_I,  FN_SRA,    Y, Y, N, N, N, X, MT_X,  N),
 
-      ADD->     List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_ADD,    Y, N, N, X, MT_X,  N),
-      SUB->     List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_SUB,    Y, N, N, X, MT_X,  N),
-      SLL->     List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_SL,     Y, N, N, X, MT_X,  N),
-      SLT->     List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_SLT,    Y, N, N, X, MT_X,  N),
-      SLTU->    List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_SLTU,   Y, N, N, X, MT_X,  N),
-      XOR->     List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_XOR,    Y, N, N, X, MT_X,  N),
-      SRL->     List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_SR,     Y, N, N, X, MT_X,  N),
-      SRA->     List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_SRA,    Y, N, N, X, MT_X,  N),
-      OR->      List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_OR,     Y, N, N, X, MT_X,  N),
-      AND->     List(Y, N, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_AND,    Y, N, N, X, MT_X,  N),
+      ADD->       List(Y, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_ADD,    Y, Y, Y, N, N, X, MT_X,  N),
+      SUB->       List(Y, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_SUB,    Y, Y, Y, N, N, X, MT_X,  N),
+      SLL->       List(Y, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_SL,     Y, Y, Y, N, N, X, MT_X,  N),
+      SLT->       List(Y, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_SLT,    Y, Y, Y, N, N, X, MT_X,  N),
+      SLTU->      List(Y, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_SLTU,   Y, Y, Y, N, N, X, MT_X,  N),
+      XOR->       List(Y, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_XOR,    Y, Y, Y, N, N, X, MT_X,  N),
+      SRL->       List(Y, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_SR,     Y, Y, Y, N, N, X, MT_X,  N),
+      SRA->       List(Y, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_SRA,    Y, Y, Y, N, N, X, MT_X,  N),
+      OR->        List(Y, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_OR,     Y, Y, Y, N, N, X, MT_X,  N),
+      AND->       List(Y, N, N, N, CSR.N, A1_RS1,  A2_RS2,  IMM_X,  FN_AND,    Y, Y, Y, N, N, X, MT_X,  N),
 
-      FENCE->   List(Y, N, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_X,      N, N, N, X, MT_X,  N), // nop
-      FENCE_I-> List(Y, N, N, N, Y, CSR.N, A1_X,    A2_X,    IMM_X,  FN_X,      N, N, N, X, MT_X,  N),
+      FENCE->     List(Y, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_X,      N, N, N, N, N, X, MT_X,  N),
+      FENCE_I->   List(Y, N, N, Y, CSR.N, A1_X,    A2_X,    IMM_X,  FN_X,      N, N, N, N, N, X, MT_X,  N),
 
-      SCALL->   List(Y, N, N, N, N, CSR.I, A1_X,    A2_X,    IMM_X,  FN_X,      N, N, N, X, MT_X,  N),
-      SBREAK->  List(Y, N, N, N, N, CSR.I, A1_X,    A2_X,    IMM_X,  FN_X,      N, N, N, X, MT_X,  N),
-      SRET->    List(Y, N, N, N, N, CSR.I, A1_X,    A2_X,    IMM_X,  FN_X,      N, N, N, X, MT_X,  N),
+      SCALL->     List(Y, N, N, N, CSR.I, A1_X,    A2_X,    IMM_X,  FN_X,      N, N, N, N, N, X, MT_X,  N),
+      SBREAK->    List(Y, N, N, N, CSR.I, A1_X,    A2_X,    IMM_X,  FN_X,      N, N, N, N, N, X, MT_X,  N),
+      SRET->      List(Y, N, N, N, CSR.I, A1_X,    A2_X,    IMM_X,  FN_X,      N, N, N, N, N, X, MT_X,  N),
 
-      CSRRW->   List(Y, N, N, N, N, CSR.W, A1_RS1,  A2_ZERO, IMM_X,  FN_ADD,    Y, N, N, X, MT_X,  N),
-      CSRRS->   List(Y, N, N, N, N, CSR.S, A1_RS1,  A2_ZERO, IMM_X,  FN_ADD,    Y, N, N, X, MT_X,  N),
-      CSRRC->   List(Y, N, N, N, N, CSR.C, A1_RS1,  A2_ZERO, IMM_X,  FN_ADD,    Y, N, N, X, MT_X,  N),
-      CSRRWI->  List(Y, N, N, N, N, CSR.W, A1_ZERO, A2_IMM,  IMM_Z,  FN_ADD,    Y, N, N, X, MT_X,  N),
-      CSRRSI->  List(Y, N, N, N, N, CSR.S, A1_ZERO, A2_IMM,  IMM_Z,  FN_ADD,    Y, N, N, X, MT_X,  N),
-      CSRRCI->  List(Y, N, N, N, N, CSR.C, A1_ZERO, A2_IMM,  IMM_Z,  FN_ADD,    Y, N, N, X, MT_X,  N),
+      CSRRW->     List(Y, N, N, N, CSR.W, A1_RS1,  A2_ZERO, IMM_X,  FN_ADD,    Y, Y, N, N, N, X, MT_X,  N),
+      CSRRS->     List(Y, N, N, N, CSR.S, A1_RS1,  A2_ZERO, IMM_X,  FN_ADD,    Y, Y, N, N, N, X, MT_X,  N),
+      CSRRC->     List(Y, N, N, N, CSR.C, A1_RS1,  A2_ZERO, IMM_X,  FN_ADD,    Y, Y, N, N, N, X, MT_X,  N),
+      CSRRWI->    List(Y, N, N, N, CSR.W, A1_ZERO, A2_IMM,  IMM_Z,  FN_ADD,    Y, N, N, N, N, X, MT_X,  N),
+      CSRRSI->    List(Y, N, N, N, CSR.S, A1_ZERO, A2_IMM,  IMM_Z,  FN_ADD,    Y, N, N, N, N, X, MT_X,  N),
+      CSRRCI->    List(Y, N, N, N, CSR.C, A1_ZERO, A2_IMM,  IMM_Z,  FN_ADD,    Y, N, N, N, N, X, MT_X,  N),
 
-      MUL->     List(Y, N, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_MUL,    N, Y, N, X, MT_X,  Y),
-      MULH->    List(Y, N, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_MULH,   N, Y, N, X, MT_X,  Y),
-      MULHU->   List(Y, N, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_MULHU,  N, Y, N, X, MT_X,  Y),
-      MULHSU->  List(Y, N, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_MULHSU, N, Y, N, X, MT_X,  Y),
-      DIV->     List(Y, N, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_DIV,    N, Y, N, X, MT_X,  Y),
-      DIVU->    List(Y, N, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_DIVU,   N, Y, N, X, MT_X,  Y),
-      REM->     List(Y, N, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_REM,    N, Y, N, X, MT_X,  Y),
-      REMU->    List(Y, N, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_REMU,   N, Y, N, X, MT_X,  Y))
+      MUL->       List(Y, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_MUL,    N, Y, Y, Y, N, X, MT_X,  Y),
+      MULH->      List(Y, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_MULH,   N, Y, Y, Y, N, X, MT_X,  Y),
+      MULHU->     List(Y, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_MULHU,  N, Y, Y, Y, N, X, MT_X,  Y),
+      MULHSU->    List(Y, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_MULHSU, N, Y, Y, Y, N, X, MT_X,  Y),
+      DIV->       List(Y, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_DIV,    N, Y, Y, Y, N, X, MT_X,  Y),
+      DIVU->      List(Y, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_DIVU,   N, Y, Y, Y, N, X, MT_X,  Y),
+      REM->       List(Y, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_REM,    N, Y, Y, Y, N, X, MT_X,  Y),
+      REMU->      List(Y, N, N, N, CSR.N, A1_X,    A2_X,    IMM_X,  FN_REMU,   N, Y, Y, Y, N, X, MT_X,  Y))
 
   val cs = DecodeLogic(io.dpath.inst, decode_default, decode_table)
 
-  val (_id_inst_valid: Bool) :: (id_j: Bool) :: (id_br: Bool) :: (id_shift_imm: Bool) :: cs1 = cs
+  val (_id_inst_valid: Bool) :: (id_j: Bool) :: (id_br: Bool) :: cs1 = cs
   val (id_fence_i: Bool) :: _id_csr :: cs2 = cs1
-  val id_sel_alu1 :: id_sel_alu2 :: id_sel_imm :: id_fn_alu :: (id_wen: Bool) :: cs3 = cs2
+  val id_sel_alu1 :: id_sel_alu2 :: id_sel_imm :: id_fn_alu :: (id_wen: Bool) :: (id_ren1: Bool) :: (id_ren2: Bool) :: cs3 = cs2
   val (id_set_sb: Bool) :: (id_mem_valid: Bool) :: (id_mem_rw: Bool) :: id_mem_type :: (id_mul_valid: Bool) :: Nil = cs3
 
   // ll stands for long-latency
@@ -184,9 +184,7 @@ class Control extends Module with ZscaleParameters
   val id_dmem_stall = io.dpath.id.mem_valid && !io.dmem.hready
   val id_mul_stall = io.dpath.id.mul_valid && !io.dpath.mul_ready
 
-  // we need this because we're using RV64I's shift instruction format
-  val id_shift_valid = !id_shift_imm || io.dpath.inst(25) === Bits(0) // checking whether shamt's bit 6 is a zero
-  val id_inst_valid = _id_inst_valid && id_shift_valid
+  val id_inst_valid = _id_inst_valid
   val id_ok = !id_sb_stall && id_valid && id_inst_valid
 
   def checkExceptions(x: Seq[(Bool, UInt)]) =
